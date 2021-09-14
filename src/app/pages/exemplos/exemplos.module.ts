8869074e07a0dab5d3390e0d44b4221c6bec1eeb
Marcelo Rezende <malvre@gmail.com>
2021-09-14 14:47:01 -0300

    kill line gaps

--- src/app/pages/exemplos/exemplos.module.ts
+++ src/app/pages/exemplos/exemplos.module.ts
@@ -1,20 +1,19 @@
-import { NgModule } from '@angular/core'
+import { NgModule } from '@angular/core';
 
-import { ExemplosRoutingModule } from './exemplos-routing.module'
+import { ExemplosRoutingModule } from './exemplos-routing.module';
 
-import { DialogsComponent } from './dialogs/dialogs.component'
-import { AutocompleteComponent } from './autocomplete/autocomplete.component'
-import { DatatableComponent } from './datatable/datatable.component'
-import { EventEmitterComponent } from './event-emitter/event-emitter.component'
+import { DialogsComponent } from './dialogs/dialogs.component';
+import { AutocompleteComponent } from './autocomplete/autocomplete.component';
+import { DatatableComponent } from './datatable/datatable.component';
+import { EventEmitterComponent } from './event-emitter/event-emitter.component';
 
-import { FormularioComponent } from './formulario/formulario.component'
-import { SharedModule } from 'src/app/shared/shared.module'
-import { NgxMaskModule } from 'ngx-mask'
-import { DashboardComponent } from './dashboard/dashboard.component'
-import { FormModalComponent } from './datatable/form-modal/form-modal.component'
-import { HeaderComponent } from './event-emitter/header/header.component'
-import { SearchPipe } from 'src/app/core/pipes/search.pipe'
-
+import { FormularioComponent } from './formulario/formulario.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { NgxMaskModule } from 'ngx-mask';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormModalComponent } from './datatable/form-modal/form-modal.component';
+import { HeaderComponent } from './event-emitter/header/header.component';
+import { SearchPipe } from 'src/app/core/pipes/search.pipe';
 
 @NgModule({
   declarations: [
@@ -26,8 +25,8 @@ import { SearchPipe } from 'src/app/core/pipes/search.pipe'
     FormularioComponent,
     FormModalComponent,
     HeaderComponent,
-    SearchPipe
+    SearchPipe,
   ],
   imports: [SharedModule, ExemplosRoutingModule, NgxMaskModule.forChild()],
 })
-export class ExemplosModule { }
+export class ExemplosModule {}
