fd994eeeb372e43f83c4dd789ef28aa2010f812b
Marcelo Rezende <malvre@gmail.com>
2021-09-14 14:37:50 -0300

    semantic refactoring

--- src/app/pages/exemplos/event-emitter/header/header.component.ts
+++ src/app/pages/exemplos/event-emitter/header/header.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnDestroy, OnInit } from '@angular/core'
-import { Subscription } from 'rxjs'
-import { MessageService } from 'src/app/core/services/message.service'
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { MessageService } from 'src/app/core/services/message.service';
 
 @Component({
   selector: 'app-header',
@@ -8,25 +8,25 @@ import { MessageService } from 'src/app/core/services/message.service'
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit, OnDestroy {
-  valorBadge: number
+  qtdMessagesBadge: number;
 
-  messages$: Subscription
+  messages$: Subscription;
 
   constructor(private messageService: MessageService) {
-    this.valorBadge = 0
+    this.qtdMessagesBadge = 0;
 
     this.messages$ = this.messageService.qtdeMessagesObserver.subscribe(
       (val) => {
-        this.valorBadge = val
+        this.qtdMessagesBadge = val;
       }
-    )
+    );
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
   ngOnDestroy(): void {
     if (this.messages$) {
-      this.messages$.unsubscribe()
+      this.messages$.unsubscribe();
     }
   }
 }
